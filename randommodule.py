d3e4ca6abb9848e112e7cbc15db0e7c341dc829d
p6anav-d-inaka6 <pranavdinakar83@gmail.com>
2024-09-23 15:45:59 +0530

    first commit~

---
+++ randommodule.py
@@ -0,0 +1,12 @@
+import random
+from random import sample
+random_varible=random.random()
+random_int_variable(111111,999999)
+random_range_variable=random.rectangle(100 ,1000 ,2)
+uniform_random_variable=random.uniform(3,5)
+
+sample_list=[1,2,3,4,6]
+choice_variable=random.choice(sample_list)
+random.shuffle(sample_list)
+
+print(sample_list)
