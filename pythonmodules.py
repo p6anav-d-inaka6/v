9f173edc8f3e9c538c8d2046ca448647ed8c6d7b
p6anav-d-inaka6 <pranavdinakar83@gmail.com>
2024-09-23 14:00:56 +0530

    first commit

---
+++ pythonmodules.py
@@ -0,0 +1,21 @@
+import math
+from math import factorial, degrees, radians
+
+examplevariable=math.sqrt(16)
+powervariable=math.pow(2  , 5)
+ceilvariable=math.ceil(3,4)
+floorvariable=math.floor(5,5)
+fabsvariable=math.fabs(-4)
+factorialvariable=math.factorial(5)
+sin_varible=math.sin(math.pi/2)
+cos_variable=math.cos(math.pi/2)
+tan_variable=math.tan(math.pi/2)
+log_varible=math.log2(3)
+log10_varibale=math.log10(3)
+degrees_varible=(math.degrees(math.pi/2))
+radians_variable=math.radians(90)
+euler_variblae=math.e
+infinity_variable=math.inf
+nan_varible=math.nan
+gcd_variable=math.gcd( 4,5 )
+lcm_varible=math.lcm(3,6)
